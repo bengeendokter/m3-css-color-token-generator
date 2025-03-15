a6152e5b9379cc40fc003a7313e4c5ea94c0f3a9
bengeendokter <benifit.arts@gmail.com>
2025-03-15 10:06:20 +0100

    Fix tokens

--- src/demo.ts
+++ src/demo.ts
@@ -2,3 +2,6 @@ import { setMetaThemeColor } from "./setThemeFromHexColor";
 
 setMetaThemeColor();
 document.documentElement.classList.add('light');
+// document.documentElement.classList.add('light-high-contrast');
+// document.documentElement.classList.add('dark');
+// document.documentElement.classList.add('dark-high-contrast');
